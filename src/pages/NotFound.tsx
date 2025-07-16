7138fa5520182456523e966eb79f7a5995a6cf02
Nafees S <nafees.s2005@gmail.com>
2025-07-16 23:07:29 +0530

    Updated NotFound Page with better visuals.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -12,16 +12,20 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-black-100">
       <div className="text-center">
         <img
           src="../images/error_icon.png"
           alt="Page Not Found"
           className="w-1/4 mb-6 mx-auto"
         />
-        <h1 className="text-6xl text-gray-600 font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! You explored too far!</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
+        <h1 className="text-6xl text-white-600 font-bold mb-4">404</h1>
+        <p className="text-xl text-white-600 font-bold mb-4">Oops! Deadlock Detected!</p>
+        <p className="text-lg text-gray-400 mb-6">
+          The page you are looking for does not exist or has been moved.<br />
+          Please check the URL or return to the home page.
+        </p>
+        <a href="/" className="text-green-500 hover:text-blue-700 underline">
           Return to Home
         </a>
       </div>
